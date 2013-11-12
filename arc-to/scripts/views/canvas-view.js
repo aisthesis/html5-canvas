65cbf497ce311f7642a3ffe67e7c12831034230c
Marshall Farrier <info@marshallfarrier.com>
2013-11-12 06:02:53 -0800

    arcTo() experimentation.

---
+++ arc-to/scripts/views/canvas-view.js
@@ -0,0 +1,34 @@
+var codeMelon = codeMelon || {}; 
+codeMelon.games = codeMelon.games || {}; 
+
+(function(_cg) {
+    _cg.CanvasView = Backbone.View.extend({
+        initialize: function(options) {
+            _.bindAll(this,
+                'render',
+                'setConstants'
+            );
+
+            this.setConstants(options);
+            this.render();
+        },
+
+        render: function() {
+            var startX = 32,
+                startY = 32,
+                width = 128,
+                radius = 32;
+
+            this.CONTEXT.strokeStyle = "#575";
+            this.CONTEXT.moveTo(startX, startY);
+            this.CONTEXT.lineTo(startX + width - radius, startY); 
+            this.CONTEXT.arcTo(startX + width, startY, startX + width, startY + radius, radius);
+            this.CONTEXT.lineTo(startX + width, startY + width);
+            this.CONTEXT.stroke();
+        },
+
+        setConstants: function(options) {
+            this.CONTEXT = this.el.getContext('2d');
+        }
+    });
+})(codeMelon.games);
