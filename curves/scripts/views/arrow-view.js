653e3a0d0401c39b72a2188c5f0cd66b3f3d3ac7
Marshall Farrier <info@marshallfarrier.com>
2013-11-14 01:21:17 -0800

    Finish arrow drawing.

--- curves/scripts/views/arrow-view.js
+++ curves/scripts/views/arrow-view.js
@@ -7,7 +7,10 @@ codeMelon.games = codeMelon.games || {};
         initialize: function(options) {
             _.bindAll(this,
                 'render',
-                'setConstants'
+                'setConstants',
+                'drawPoint',
+                'drawBezierPoints',
+                'drawArrow'
             );
 
             this.setConstants(options);
@@ -15,27 +18,34 @@ codeMelon.games = codeMelon.games || {};
         },
 
         render: function() {
-            this.CONTEXT.strokeStyle = 'yellow';
-            this.CONTEXT.shadowColor = 'rgba(50, 50, 50, 1.0)';
-            this.CONTEXT.shadowOffsetX = 2;
-            this.CONTEXT.shadowOffsetY = 2;
-            this.CONTEXT.shadowBlur = 4;
-            this.CONTEXT.lineWidth = 20;
-            this.CONTEXT.lineCap = 'round';
-            
-            this.CONTEXT.beginPath();
-            this.CONTEXT.moveTo(this.START.getX(), this.START.getY());
-            this.CONTEXT.quadraticCurveTo(this.CONTROL1.getX(), this.CONTROL1.getY(),
-                this.ANCHOR1.getX(), this.ANCHOR1.getY());
-            this.CONTEXT.quadraticCurveTo(this.CONTROL2.getX(), this.CONTROL2.getY(),
-                this.ANCHOR2.getX(), this.ANCHOR2.getY());
-            this.CONTEXT.quadraticCurveTo(this.CONTROL3.getX(), this.CONTROL3.getY(),
-                this.ANCHOR3.getX(), this.ANCHOR3.getY());
-            this.CONTEXT.stroke();
+            this.drawArrow();
+            this.drawBezierPoints();
         },
 
         setConstants: function(options) {
             this.CONTEXT = this.el.getContext('2d');
+            this.ARROW_MARGIN = 30;
+            this.POINT_RADIUS = 7;
+            this.CORNERS = [
+                {
+                    ctrl: new Point(this.el.width - this.ARROW_MARGIN, 
+                        this.el.height - this.ARROW_MARGIN),
+                    anchor: new Point(this.el.width - this.ARROW_MARGIN * 2,
+                        this.el.height - this.ARROW_MARGIN)
+                },
+                {
+                    ctrl: new Point(this.POINT_RADIUS,
+                        this.el.height / 2),
+                    anchor: new Point(this.ARROW_MARGIN,
+                        this.el.height / 2 - this.ARROW_MARGIN)
+                },
+                {
+                    ctrl: new Point(this.el.width - this.ARROW_MARGIN,
+                        this.ARROW_MARGIN),
+                    anchor: new Point(this.el.width - this.ARROW_MARGIN,
+                        this.ARROW_MARGIN * 2)
+                }
+            ];
             this.START = new Point(120.5, 130);
             this.ANCHOR1 = new Point(160.6, 150.5);
             this.CONTROL1 = new Point(150.8, 130);
@@ -43,6 +53,48 @@ codeMelon.games = codeMelon.games || {};
             this.CONTROL2 = new Point(190, 250.0);
             this.ANCHOR3 = new Point(290, 70.5);
             this.CONTROL3 = new Point(240, 100.5);
+        },
+
+        drawPoint: function(point, strokeStyle, fillStyle) {
+            this.CONTEXT.beginPath();
+            this.CONTEXT.fillStyle = fillStyle;
+            this.CONTEXT.strokeStyle = strokeStyle;
+            this.CONTEXT.lineWidth = 1;
+            this.CONTEXT.arc(point.getX(), point.getY(), this.POINT_RADIUS, 
+                    0, Math.PI * 2, false);
+            this.CONTEXT.fill();
+            this.CONTEXT.stroke();
+        },
+
+        drawBezierPoints: function() {
+            var i,
+                strokeStyle,
+                fillStyle;
+
+            for (i = 0; i < this.CORNERS.length; i++) {
+                this.drawPoint(this.CORNERS[i].ctrl, 'blue', 'white');
+                this.drawPoint(this.CORNERS[i].anchor, 'white', 'blue');
+            }
+        },
+
+        drawArrow: function() {
+            this.CONTEXT.strokeStyle = 'white';
+            this.CONTEXT.fillStyle = 'blue';
+
+            this.CONTEXT.moveTo(this.el.width - this.ARROW_MARGIN, 
+                    this.ARROW_MARGIN * 2);
+            this.CONTEXT.lineTo(this.el.width - this.ARROW_MARGIN,
+                    this.el.height - this.ARROW_MARGIN * 2);
+            this.CONTEXT.quadraticCurveTo(this.CORNERS[0].ctrl.getX(), this.CORNERS[0].ctrl.getY(),
+                    this.CORNERS[0].anchor.getX(), this.CORNERS[0].anchor.getY());
+            this.CONTEXT.lineTo(this.ARROW_MARGIN, this.el.height / 2 + this.ARROW_MARGIN);
+            this.CONTEXT.quadraticCurveTo(this.CORNERS[1].ctrl.getX(), this.CORNERS[1].ctrl.getY(),
+                    this.CORNERS[1].anchor.getX(), this.CORNERS[1].anchor.getY());
+            this.CONTEXT.lineTo(this.el.width - this.ARROW_MARGIN * 2, this.ARROW_MARGIN);
+            this.CONTEXT.quadraticCurveTo(this.CORNERS[2].ctrl.getX(), this.CORNERS[2].ctrl.getY(),
+                    this.CORNERS[2].anchor.getX(), this.CORNERS[2].anchor.getY());
+            this.CONTEXT.fill();
+            this.CONTEXT.stroke();
         }
     });
 })(codeMelon.games);
