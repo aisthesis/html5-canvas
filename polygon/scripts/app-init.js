5a7db18f47fea5b0c642120d4b1e44417aba2102
Marshall Farrier <info@marshallfarrier.com>
2013-11-14 23:01:00 -0800

    Setup for draggable polygon.

--- polygon/scripts/app-init.js
+++ polygon/scripts/app-init.js
@@ -5,4 +5,7 @@ var app = app || {};
     app.basicPolygonView = new _cg.BasicPolygonView({
         el: $('#basic-polygon')
     });
+    app.draggablePolygonView = new _cg.DraggablePolygonView({
+        el: $('#draggable-polygon')
+    });
 })(codeMelon.games);
---
+++ polygon/scripts/views/draggable-polygon-view.js
@@ -0,0 +1,43 @@
+var codeMelon = codeMelon || {}; 
+codeMelon.games = codeMelon.games || {}; 
+
+(function(_cg) {
+    "use strict";
+    _cg.DraggablePolygonView = Backbone.View.extend({
+        initialize: function(options) {
+            _.bindAll(this,
+                'render',
+                'setConstants'
+            );
+
+            this.setConstants(options);
+            this.render();
+        },
+
+        render: function() {
+            var polygon = new Polygon({
+                center: new Point(this.el.width / 2, this.el.height / 2),
+                radius: this.el.width / 4,
+                sides: 8,
+                angle: Math.PI / 2,
+                fillStyle: 'cornflowerblue',
+                strokeStyle: 'goldenrod',
+                lineWidth: 8
+            });
+
+            polygon.draw(this.CONTEXT);
+            polygon.lineWidth = 2;
+            polygon.strokeStyle = 'red';
+            polygon.stroke(this.CONTEXT);
+            polygon.radius = polygon.radius / 2;
+            polygon.angle = Math.PI / 8;
+            polygon.resetVertices();
+            polygon.fillStyle = 'saddlebrown';
+            polygon.fill(this.CONTEXT);
+        },
+
+        setConstants: function(options) {
+            this.CONTEXT = this.el.getContext('2d');
+        }
+    });
+})(codeMelon.games);
