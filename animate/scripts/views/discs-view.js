099a25c47b0ce0b2349d89a42f488dfa46e8ea0f
Marshall Farrier <info@marshallfarrier.com>
2013-11-30 19:24:37 -0800

    Basic animation of bouncing balls.

--- animate/scripts/views/discs-view.js
+++ animate/scripts/views/discs-view.js
@@ -6,15 +6,20 @@ _c.app.views = _c.app.views || {};
     "use strict";
     views.DiscsView = Backbone.View.extend({
         initialize: function(options) {
+            var _this = this;
+
             _.bindAll(this,
                 'render',
                 'setConstants',
-                'initMembers'
+                'initMembers',
+                'update',
+                'animate'
             );
 
             this.setConstants(options);
             this.initMembers(options);
             this.render();
+            requestAnimationFrame(_this.animate);
         },
 
         render: function() {
@@ -25,6 +30,8 @@ _c.app.views = _c.app.views || {};
 
         setConstants: function(options) {
             this.RADIUS = 32;
+            this.X_MAX = this.el.width - this.RADIUS;
+            this.Y_MAX = this.el.height - this.RADIUS;
         },
 
         initMembers: function(options) {
@@ -49,7 +56,7 @@ _c.app.views = _c.app.views || {};
                         strokeStyle: 'orange'
                     }),
 
-                    velocity: new _c.draw.Vector(-3.2, 3.5)
+                    velocity: new _c.draw.Vector(2.2, 2.5)
                 },
 
                 {
@@ -60,9 +67,34 @@ _c.app.views = _c.app.views || {};
                         strokeStyle: 'navy'
                     }),
 
-                    velocity: new _c.draw.Vector(-3.2, 3.5)
+                    velocity: new _c.draw.Vector(1.2, 1.5)
                 },
             ];
+        },
+
+        update: function() {
+            var _this = this,
+                tmp;
+
+            this.discs.forEach(function(disc) {
+                // reverse directions if edge would be hit
+                tmp = disc.circle.center.x + disc.velocity.x;
+                if (tmp < _this.RADIUS || _this.X_MAX < tmp) {
+                    disc.velocity.x = -disc.velocity.x;
+                }
+                tmp = disc.circle.center.y + disc.velocity.y; 
+                if (tmp < _this.RADIUS || _this.Y_MAX < tmp) {
+                    disc.velocity.y = -disc.velocity.y;
+                }
+                disc.circle.center.move(disc.velocity);
+            });
+        },
+
+        animate: function(time) {
+            this.context.clearRect(0, 0, this.el.width, this.el.height);
+            this.update();
+            this.render();
+            requestAnimationFrame(this.animate);
         }
     });
 })(_c.app.views);
