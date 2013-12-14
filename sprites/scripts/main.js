844872c7e587280b654afb6958372eb65a2e1b84
Marshall Farrier <info@marshallfarrier.com>
2013-12-13 18:39:56 -0800

    Tile set.

--- sprites/scripts/main.js
+++ sprites/scripts/main.js
@@ -10,6 +10,7 @@ require.config({
         'SimpleView': 'scripts/views/simple-view',
         'ClockView': 'scripts/views/clock-view',
         'BombView': 'scripts/views/bomb-view',
+        'RunnerView': 'scripts/views/runner-view',
         'jquery': '../lib/jquery-2.0.3.min',
         'bootstrap': '../lib/bootstrap-3.0.0/js/bootstrap.min',
         'underscore': '../lib/underscore-1.5.2.min',
@@ -18,18 +19,19 @@ require.config({
     },
 
     shim: {
-        'SimpleView': ['Backbone'],
-        'ClockView': ['Backbone'],
-        'BombView': ['Backbone'],
-        'Backbone': ['underscore', 'jquery'],
+        'SimpleView': ['Backbone', 'draw'],
+        'ClockView': ['Backbone', 'draw'],
+        'BombView': ['Backbone', 'draw'],
+        'RunnerView': ['Backbone', 'draw'],
+        'Backbone': ['underscore', 'jquery', 'draw'],
         'bootstrap': ['jquery']
     }
 });
 
 require(['jquery', 'bootstrap', 'underscore', 'Backbone', 'draw', 
-    'SimpleView', 'ClockView', 'BombView'], 
+    'SimpleView', 'ClockView', 'BombView', 'RunnerView'], 
     function($, bootstrap, _, Backbone, draw, 
-        SimpleView, ClockView, BombView) {
+        SimpleView, ClockView, BombView, RunnerView) {
         'use strict';
         _c.app.main = {};
         _c.app.main.simpleView = new _c.app.views.SimpleView({
@@ -40,5 +42,8 @@ require(['jquery', 'bootstrap', 'underscore', 'Backbone', 'draw',
         }); 
         _c.app.main.bombView = new _c.app.views.BombView({
             el: '#bomb'
+        }); 
+        _c.app.main.runnerView = new _c.app.views.RunnerView({
+            el: '#runner'
         }); 
     });
