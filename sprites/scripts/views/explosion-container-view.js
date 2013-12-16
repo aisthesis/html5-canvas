e59a48dbdfc540797a73e1d09b74eb941dfea2f0
Marshall Farrier <info@marshallfarrier.com>
2013-12-16 00:59:23 -0800

    Implement bomb sprite using FrameSet for drawable.

--- sprites/scripts/views/explosion-container-view.js
+++ sprites/scripts/views/explosion-container-view.js
@@ -5,11 +5,16 @@ _c.app.views = _c.app.views || {};
 (function(views) {
     "use strict";
     views.ExplosionContainerView = Backbone.View.extend({
+        events: {
+            'click #light-fuse': 'handleLightFuse'
+        },
+
         initialize: function(options) {
             _.bindAll(this,
                 'render',
                 'setConstants',
-                'initMembers'
+                'initMembers',
+                'handleLightFuse'
             );
 
             this.setConstants(options);
@@ -29,6 +34,11 @@ _c.app.views = _c.app.views || {};
             this.explosionView = new _c.app.views.ExplosionView({
                 el: '#explosion'
             });
+        },
+
+        handleLightFuse: function(event) {
+            this.explosionView.ready = true;
+            this.explosionView.animate();
         }
     });
 })(_c.app.views);
--- sprites/scripts/views/explosion-view.js
+++ sprites/scripts/views/explosion-view.js
@@ -10,12 +10,12 @@ _c.app.views = _c.app.views || {};
                 'render',
                 'setConstants',
                 'initMembers',
-                'animate'
+                'animate',
+                'getFrames'
             );
 
             this.setConstants(options);
             this.initMembers(options);
-            this.render();
         },
 
         render: function() {
@@ -26,34 +26,21 @@ _c.app.views = _c.app.views || {};
             this.OFFSET = new _c.draw.Vector(-22, -32);
             // ms to expire before moving to next image
             this.PAGEFLIP_INTERVAL = 100;
+            this.FUSE_FRAMES = 9;
+            this.EXPLOSION_FRAMES = 9;
         },
 
         initMembers: function(options) {
-            var _this = this;
+            var _this = this,
+                params = {
+                    frames: this.getFrames()
+                };
 
             this.context = this.el.getContext('2d');
             this.lastAdvance = new Date();
+            this.ready = false;
             this.sprite = new _c.draw.Sprite({
-                drawable: new _c.draw.TileSet({
-                    src: 'running-sprite-sheet.png',
-                    corner: new _c.draw.Point(_this.el.width / 2, 
-                        _this.el.height / 2).offset(_this.OFFSET),
-                    onload: function(event) {
-                        _this.render();
-                        _this.animate();
-                    },
-                    cells: [
-                        { corner: new _c.draw.Point(0, 0), width: 47, height: 64 },
-                        { corner: new _c.draw.Point(55, 0), width: 44, height: 64 },
-                        { corner: new _c.draw.Point(107, 0), width: 39, height: 64 },
-                        { corner: new _c.draw.Point(150, 0), width: 46, height: 64 },
-                        { corner: new _c.draw.Point(208, 0), width: 49, height: 64 },
-                        { corner: new _c.draw.Point(265, 0), width: 46, height: 64 },
-                        { corner: new _c.draw.Point(320, 0), width: 42, height: 64 },
-                        { corner: new _c.draw.Point(380, 0), width: 35, height: 64 },
-                        { corner: new _c.draw.Point(425, 0), width: 35, height: 64 },
-                    ]
-                }),
+                drawable: new _c.draw.FrameSet(params),
 
                 paint: function(context) {
                     this.drawable.draw(context);
@@ -72,8 +59,57 @@ _c.app.views = _c.app.views || {};
         },
 
         animate: function(time) {
-            this.render();
-            requestAnimationFrame(this.animate);
+            var _this = this;
+
+            if (this.ready) {
+                if (this.sprite.drawable.index === this.sprite.drawable.frames.length - 1) {
+                    this.ready = false;
+                    setTimeout(function() {
+                        _this.render();
+                    }, this.PAGEFLIP_INTERVAL * 8);
+                }
+                this.render();
+                requestAnimationFrame(this.animate);
+            }
+        },
+
+        getFrames: function() {
+            var _this = this,
+                frames = [],
+                paramsArray = [],
+                _width = 180,
+                _height = 130,
+                corner =  new _c.draw.Point(this.el.width / 2, 
+                    this.el.height / 2).offset({x: -_width / 2, y: -_height / 2});
+
+            // initial bomb image
+            paramsArray.push({ src: 'bomb.png' });
+
+            // fuse burning
+            for (var i = 0; i < this.FUSE_FRAMES; i++) {
+                paramsArray.push({ src: 'explosion/fuse-0' + i + '.png' });
+            }
+
+            // bomb no fuse
+            paramsArray.push({ src: 'explosion/bomb-no-fuse.png' });
+           
+            // post-explosion
+            for (var i = 0; i < this.EXPLOSION_FRAMES; i++) {
+                paramsArray.push({ src: 'explosion/explosion-0' + i + '.png' });
+            }
+
+            paramsArray.forEach(function(params) {
+                params.corner = new _c.draw.Point(corner.x, corner.y),
+                params.width = _width,
+                params.height = _height,
+                params.onload = function(event) {
+                    _this.ready = true;
+                    _this.sprite.paint(_this.context);
+                }
+                frames.push(new _c.draw.Image(params));
+            });
+
+            return frames;
         }
     });
 })(_c.app.views);
